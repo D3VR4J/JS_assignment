90cd41e8d989ad1bbe45d395361ca65a75d7b1b0
D3VR4J <33987984+D3VR4J@users.noreply.github.com>
2022-03-03 03:35:28 +0530

    Day1

---
+++ Day1Assignment/index.js
@@ -0,0 +1,18 @@
+var name = prompt("What is your Name?");
+        var age = prompt("What is your Age?");
+
+        if(age>=18) { 
+        alert(name + "is eligible for voting");
+        }
+        else {
+        alert(name + "you are Not Eligible to vote You are inder Age")
+        }
+// t check if the number is even or not
+var number = prompt("Inpu a number to check even or not!")
+number = number % 2
+if(number==0){
+        alert("This number is a even number")
+}
+else {
+        alert("This number is not a even number")
+}
